0799601b4ac269dca7df57ae5b0a7fea70463887
Vince Andrew Pimentel <contact@vincep.me>
2019-01-12 01:03:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,10 +25,10 @@ class ApplicationController < Sinatra::Base
   get '/account' do
     user = User.find(session[:user_id])
 
-    if user
-      erb :account
-    else
+    if user.to_s.include?("")
       erb :error
+    else
+      erb :account
     end
   end
 
